2c272d4c21531eff42263c1fe24b19181a37eb9a
Elizabeth Rp <tigeroo22@gmail.com>
2022-02-15 18:16:53 -0600

    code from module 1

---
+++ module1.py
@@ -0,0 +1,37 @@
+# Module 1
+# Elizabeth Roma
+
+# int
+num1 = 1
+
+print(num1)
+
+
+# float
+num2 = 1.1
+
+print(num2)
+
+
+# boolean
+status = True
+
+print(status)
+
+
+# string
+mystr = "Hello class"
+
+print(mystr)
+
+
+# tuple
+mytup = (1, 2, 3, 4)
+
+print(mytup)
+
+
+# dictionary
+mydict = {"key1": 1, "key2":2, "key3":3}
+
+print(mydict)
